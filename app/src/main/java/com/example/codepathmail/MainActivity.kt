6d972a2566a25d508879e7f2581f1622e8edaefc
smac330 <shakaylahmacklin330@gmail.com>
2025-02-07 23:18:54 -0500

    inital test

---
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.codepathmail
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRV)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails
+            val newEmails = EmailFetcher.getNext5Emails()
+            // Add new emails to existing list of emails
+            (emails as MutableList<Email>).addAll(newEmails)
+            // Notify the adapter there's new emails so the RecyclerView layout is updated
+            adapter.notifyDataSetChanged()
+        }
+
+    }
+}
